8817cc965acca938abcd8e44eefa32fed28c47fd
Kendra Nixon <knixon000@nyc.yearup.org>
2018-11-13 16:56:02 -0500

    some object oriented javascript

--- project-2/main.js
+++ project-2/main.js
@@ -1,1 +1,16 @@
+class Pokedex {
+  constructor(url, classnames){
+    this.url = url;
+    this.classnames = classnames;
+    this.image = document.createElement('img');
+    this.trainer = document.getElementById('pokeball-center');
+  }
+  execute(){
+    this.trainer.appendChild(this.image);
+    this.image.src=this.url;
+    this.image.classNames = this.classnames;
+  }
+}
 
+let Kendra = new Pokedex("trainer.png", "trainer");
+Kendra.execute();
